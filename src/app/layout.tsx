e4ded048c0bc9f5517827c8604cb958e74b276bb
Jake Shaw <jakeshaw121@gmail.com>
2025-04-22 09:26:44 +0100

    updated landing page and added not found/loading state

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Navbar } from "@/components/Navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Jake Shaw Portfolio",
+  description: "Portfolio website created using Nextjs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex`}
+      >
+        <header>
+          <Navbar />
+        </header>
+        <div className="ml-64 p-4 flex-1">{children}</div>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function errorPage() {
+  return (
+    <div className="flex flex-col max-w-4/12 text-center  h-screen ml-120 mt-60">
+      <p className="text-8xl">404</p>
+      <em>
+        &quot;For the last time, I didn&apos;t break the link! I was nowhere
+        near the link when it broke!&quot;
+      </em>
+    </div>
+  );
+}
